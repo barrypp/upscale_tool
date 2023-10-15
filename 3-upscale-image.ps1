e971e48d1846b77f335d52380500b0428afbde77
Barrypp.zzx <4166765+barrypp@users.noreply.github.com>
2023-10-15 22:45:40 +0800

    add 3-upscale-video

---
+++ 3-upscale-image.ps1
@@ -0,0 +1,40 @@
+$env:Path = 'C:\Program Files\7-Zip;C:\ProgramFiles\mpv;' + $env:Path
+
+
+ls -Filter '1/*.cbz' | ForEach-Object {
+    7z x $_.FullName -o"2/tmp"
+    mkdir -Force 3/tmp
+
+    #
+    ls -Filter './2/tmp/*.gif' | ForEach-Object {
+        $a = split-path $_ -LeafBase
+        ffmpeg -hide_banner -y -i "$_" -c:v hevc_nvenc -preset p7 -pix_fmt p010le -profile:v main10 -b:v 0K "./2/tmp/$a.mkv"
+        cmd /c "vspipe -c y4m --arg in=./2/tmp/$a.mkv --arg is_img=False upscale_and_rife_2.vpy - | ffmpeg -hide_banner -y -i - -c:v hevc_nvenc -preset p7 -pix_fmt p010le -profile:v main10 -b:v 0K ./3/tmp/$a.mkv"
+        rm $_
+        rm ./2/tmp/$a.mkv*
+    }
+
+    #
+    identify ./2/tmp/* | ConvertFrom-Csv -Delimiter " " -Header a,b,c,d,e,f | foreach {
+        $_.a | Out-File -FilePath tmp.$($_.c)_$($_.f).txt -Append
+    }
+
+    #
+    ls -Filter 'tmp.*.txt' | ForEach-Object {
+        vspipe -p --arg "in=$($_.Name)" --arg is_img=True upscale_and_rife_2.vpy .
+        $b = 0
+        Get-Content $_.Name | % {
+            $c = split-path $_ -LeafBase
+            mv 3/tmp/tmp_$b.bmp 3/tmp/$c.bmp
+            $b = $b + 1
+        }
+    }
+    
+    7z a -mx0 -tzip "3/$($_.Name)" ./3/tmp/*
+
+    rm -R -Force 2/tmp
+    rm -R -Force 3/tmp
+    rm -Force tmp.*.txt
+}
+
+Read-Host -Prompt "Press any key to continue"
---
+++ 3-upscale-video.ps1
@@ -0,0 +1,14 @@
+$env:Path = 'C:\Program Files\7-Zip;C:\ProgramFiles\mpv;' + $env:Path
+
+$test_run = $false
+$s = If ($test_run) {"2"} Else {"1"}
+
+ls -Filter '1/*' | ForEach-Object {
+    $a = split-path $_ -LeafBase
+    if ($test_run){
+        ffmpeg -hide_banner -loglevel warning -y -hwaccel d3d11va -ss "00:11:05" -t "00:00:10" -i "$_" -c copy "2/$a.mkv"
+    }
+    cmd /c "vspipe -c y4m --arg ""in=$s/$a.mkv"" --arg is_img=False upscale_and_rife_2.vpy - | ffmpeg -hide_banner -y -i - -i ""$s/$a.mkv"" -map 0:v -map 1 -map -1:v -c:a copy -c:s copy -c:v hevc_nvenc -preset p7 -pix_fmt p010le -profile:v main10 -b:v 0K ""./3/$a.mkv"""
+}
+
+Read-Host -Prompt "Press any key to continue"
